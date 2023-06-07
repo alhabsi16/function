68fa504c927e4fa7cf7fbcfa165df6cd469cab26
alhabsi16 <mxmx8283@gmail.com>
2023-06-07 12:27:44 +0400

    switch1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            vowel();
+        }
+
+        static void vowel()
+            {
+            Console.Write("Please Choose one of a vowel character (a,e,i,o,u): ");
+            char character = Convert.ToChar(Console.ReadLine());
+
+            switch(character)
+            {
+                case 'a' or 'A':
+                    Console.WriteLine(character + " is the vowel");
+                    break;
+
+
+                case 'e' or 'E':
+                    Console.WriteLine(character + " is the vowel");
+                    break;
+
+                case 'i' or 'I':
+                    Console.WriteLine(character + " is the vowel");
+                    break;
+
+                case 'o'or 'O':
+                    Console.WriteLine(character + " is the vowel");
+                    break;
+
+                case 'u'or 'U':
+                    Console.WriteLine(character + " is the vowel");
+                    break;
+
+                default:
+                    Console.WriteLine("Consonant");
+                    break;
+            }
+
+            }
+    }
+}
